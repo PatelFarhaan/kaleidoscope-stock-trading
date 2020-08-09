d6266db4c70f9cec9c3bde75509251bfbfa89bdd
Farhaan <patel.farhaaan@gmail.com>
2020-08-09 15:23:24 -0700

    user collection updated

--- common_utilities/account_disapprove.py
+++ common_utilities/account_disapprove.py
@@ -3,15 +3,16 @@
 #<==================================================================================================>
 import sys
 sys.path.append("../")
+from pymongo import MongoClient
 from flask import redirect, url_for
+from common_utilities import CONSTANT
 from project.models import Investor, Startup
 
 
 #<==================================================================================================>
-#                                  ACCOUNT APPROVE: INV + STR
+#                                  ACCOUNT DIS-APPROVE: INV + STR
 #<==================================================================================================>
 def disapprove_account(user_email, is_inv):
-    # import ipdb; ipdb.set_trace()
     collection = Investor if is_inv else Startup
     redirect_url = 'admin.investor_account' if is_inv else 'admin.startup_account'
 
@@ -21,3 +22,24 @@ def disapprove_account(user_email, is_inv):
 
     user_obj.approved = False
     user_obj.save()
+    user_collection_update(user_email, is_inv)
+
+
+#<==================================================================================================>
+#                                   USER COLLECTION UPDATE
+#<==================================================================================================>
+def user_collection_update(email, is_inv):
+    remote_mongo_uri = CONSTANT.CURRENT_DATABASE.value
+    mongo_client = MongoClient(remote_mongo_uri)
+    db = mongo_client.matching
+    collection = db.users
+
+    my_query = {"email": email, "investor": is_inv}
+    newvalues = {"$set": {"approved": False}}
+
+    try:
+        collection.update_one(my_query, newvalues)
+    except:
+        pass
+
+    return
